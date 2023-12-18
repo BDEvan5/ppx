227bcbbf93bb55a941f9a37b5f332ed03d11e3a7
Benjamin Evans <19811799@sun.ac.za>
2023-12-18 12:43:12 +0200

    Removed network from algorithm

--- ppox/systems/ppo.py
+++ ppox/systems/ppo.py
@@ -1,59 +1,20 @@
 import jax
 import jax.numpy as jnp
 import numpy as np
-import flax.linen as nn
-from flax.linen.initializers import constant, orthogonal
 from flax.training.train_state import TrainState
 import optax
-import distrax
 import gymnax
 import time 
 import hydra
 
-from typing import Sequence, NamedTuple, Any
 from rich.pretty import pprint
 from colorama import Fore, Style
 from omegaconf import DictConfig, OmegaConf
 
 from ppox.wrappers import LogWrapper, FlattenObservationWrapper
 from ppox.types import Transition
+from ppox.network import ActorCritic
 
-class ActorCritic(nn.Module):
-    action_dim: Sequence[int]
-    activation: str = "tanh"
-
-    @nn.compact
-    def __call__(self, x):
-        if self.activation == "relu":
-            activation = nn.relu
-        else:
-            activation = nn.tanh
-        actor_mean = nn.Dense(
-            64, kernel_init=orthogonal(np.sqrt(2)), bias_init=constant(0.0)
-        )(x)
-        actor_mean = activation(actor_mean)
-        actor_mean = nn.Dense(
-            64, kernel_init=orthogonal(np.sqrt(2)), bias_init=constant(0.0)
-        )(actor_mean)
-        actor_mean = activation(actor_mean)
-        actor_mean = nn.Dense(
-            self.action_dim, kernel_init=orthogonal(0.01), bias_init=constant(0.0)
-        )(actor_mean)
-        pi = distrax.Categorical(logits=actor_mean)
-
-        critic = nn.Dense(
-            64, kernel_init=orthogonal(np.sqrt(2)), bias_init=constant(0.0)
-        )(x)
-        critic = activation(critic)
-        critic = nn.Dense(
-            64, kernel_init=orthogonal(np.sqrt(2)), bias_init=constant(0.0)
-        )(critic)
-        critic = activation(critic)
-        critic = nn.Dense(1, kernel_init=orthogonal(1.0), bias_init=constant(0.0))(
-            critic
-        )
-
-        return pi, jnp.squeeze(critic, axis=-1)
 
 
 def learn(env, network, runner_state, config, env_params, rng):
